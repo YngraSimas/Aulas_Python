59a1294d899bf5734fc78de3ca691b3811b4b78c
Aurelio Marques Vulcão <avaliacoesvulcao@gmail.com>
2019-07-25 10:07:27 -0300

    exercicios

---
+++ Aula Yngra.py
@@ -0,0 +1,15 @@
+
+x = 2
+class yngra():
+    x = 3
+    def yngra2(self):
+        y=x
+        return x
+    def yngra3(self):
+        x = 4
+        return x
+
+
+print(x)
+print(yngra.yngra2(1))
+print(yngra.yngra3(1))
