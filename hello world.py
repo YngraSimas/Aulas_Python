cd7dd450b01b248ef4601299fb46b2d1c310c76b
Aurelio Marques Vulcão <avaliacoesvulcao@gmail.com>
2019-07-25 19:48:41 -0300

    Hello world
    O Primeiro HW . Colorido pois o básico não me agrada

--- hello world.py
+++ hello world.py
@@ -3,6 +3,6 @@ import cores
 print(f"{cores.Magenta}H{cores.vermelho}e"
       f"{cores.Azul}l{cores.Amarelo}l"
       f"{cores.Cyan}o {cores.Magenta}W"
-      f"{cores.vermelho}o{cores.Azul}r"
+      f"{cores.vermelho}o{cores.Azu l}r"
       f"{cores.Amarelo}l{cores.Cyan}d")
 
