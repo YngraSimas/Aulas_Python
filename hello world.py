a89c77da8de3dbc4d54ad324d8131899670d69aa
Aurelio Marques Vulcão <avaliacoesvulcao@gmail.com>
2019-07-25 19:29:55 -0300

    Hello World

---
+++ hello world.py
@@ -0,0 +1,8 @@
+import cores
+
+print(f"{cores.Magenta}H{cores.vermelho}e"
+      f"{cores.Azul}l{cores.Amarelo}l"
+      f"{cores.Cyan}o {cores.Magenta}W"
+      f"{cores.vermelho}o{cores.Azul}r"
+      f"{cores.Amarelo}l{cores.Cyan}d")
+
